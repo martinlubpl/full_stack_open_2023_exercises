86c56115ff4e6e33edb661463a4ea2d58a6d6e42
Marcin Majewski <smdesign.eu@gmail.com>
2023-06-14 13:47:35 +0200

    1.13 votes

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -13,16 +13,30 @@ const App = () => {
   ]
    
   const [selected, setSelected] = useState(0)
+  const [points,setPoints] = useState({})
   
   // Returns a random integer from 0 to 9:
   // Math.floor(Math.random() * 10);
   const selectAnecdote = () => {
-    setSelected(Math.floor(Math.random() * anecdotes.length))
+    const randIndex = Math.floor(Math.random() * anecdotes.length)
+    console.log(randIndex)
+    setSelected(randIndex)
+  }
+  
+  // console.log(points)
+
+  const addVote = () => {
+    const copy = {...points}
+    copy[selected] = (copy[selected] || 0) + 1;
+    setPoints(copy)
+    console.log(points)
   }
 
   return (
     <div>
       <div>{anecdotes[selected]}</div>
+      <div>has {points[selected] || 0} votes</div>
+      <button onClick={addVote}>vote</button>
       <button onClick={selectAnecdote}>next anecdote</button>
     </div>
   )
