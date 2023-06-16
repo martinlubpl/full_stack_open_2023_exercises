0c247e3b6f6cee3aac21f3a8fb305da15649de21
Marcin Majewski <smdesign.eu@gmail.com>
2023-06-16 16:55:39 +0200

    2.12 axios post to db

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,58 +6,66 @@ import axios from 'axios'
 
 
 const App = () => {
-  // const [persons, setPersons] = useState([
-  //   { name: 'Arto Hellas', phone: '040-555555'},
-  //   { name: 'Ada Lovelace', phone: '39-44-5323523', id: 2 },
-  //   { name: 'Dan Abramov', phone: '12-43-234345', id: 3 },
-  //   { name: 'Mary Poppendieck', phone: '39-23-6423122', id: 4 }
-  // ]) 
 
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
   const [searchFilter, setSearchFilter] = useState('')
+  const dbUrl = 'http://localhost:3001/persons'
 
+  /////////////////////////////////////////////////
   const handleAddPerson = (event) => {
-    event.preventDefault() 
+    event.preventDefault()
     // console.log(event.target)
     //check if exists
     const personExists = persons.some(person => person.name === newName)
-    const phoneExists = persons.some(person => person.phone === newPhone)
-    if(personExists || phoneExists) {
+    const phoneExists = persons.some(person => person.number === newPhone)
+    if (personExists || phoneExists) {
       alert(`${newName} or ${newPhone} is already added to the phonebook`)
     } else {
-      const newPersons = persons.concat({name: newName, phone: newPhone})
-      setPersons(newPersons)
-      setNewName('')
-      setNewPhone('') 
+      //db
+      const newPerson = { name: newName, number: newPhone }
+      axios
+        .post(dbUrl, newPerson)
+        .then(response => {
+          //state
+          const newPersons = persons.concat(response.data)
+          setPersons(newPersons)
+          setNewName('')
+          setNewPhone('')
+        })
+      //state
+      // const newPersons = persons.concat({ name: newName, number: newPhone })
+      // setPersons(newPersons)
+      // setNewName('')
+      // setNewPhone('')
     }
     //
-    
+
   }
   useEffect(() => {
     console.log('useeffect')
-    axios.get('http://localhost:3001/persons')
-    .then(response => setPersons(response.data))
-  }, []) 
+    axios.get(dbUrl)
+      .then(response => setPersons(response.data))
+  }, [])
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter 
+      <Filter
         searchFilter={searchFilter}
         setSearchFilter={setSearchFilter} />
-      <PersonForm 
-      setNewName={setNewName}
-      newName={newName}
-      setNewPhone={setNewPhone}
-      newPhone={newPhone}
-      handleAddPerson={handleAddPerson}/>
+      <PersonForm
+        setNewName={setNewName}
+        newName={newName}
+        setNewPhone={setNewPhone}
+        newPhone={newPhone}
+        handleAddPerson={handleAddPerson} />
       <h2>Numbers</h2>
       {/* {persons.filter(person => person.name.toLowerCase().includes(searchFilter.toLowerCase())).map(person => <p key={person.name}>{person.name} {person.phone}</p>)} */}
       {/* only filtered */}
       <Persons persons={persons.filter(person => person.name.toLowerCase()
-      .includes(searchFilter.toLowerCase()))}/>
+        .includes(searchFilter.toLowerCase()))} />
     </div>
   )
 }
--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Persons = ({persons}) => {
-    // console.log(props)
-    //map(person => <p key={person.name}>{person.name} {person.phone}</p>)}
-    return (
-    persons.map(person => <p key={person.name}>{person.name} {person.phone}</p>)
+const Persons = ({ persons }) => {
+  // console.log(props)
+  //map(person => <p key={person.name}>{person.name} {person.phone}</p>)}
+  return (
+    persons.map(person => <p key={person.id}>{person.name} {person.number}</p>)
   )
 }
 
